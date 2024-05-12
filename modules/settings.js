c06f8854d2cdbab5fe559325fb659c3d09a80344
Timo Wagner <kasimos@users.noreply.github.com>
2024-05-13 00:18:23 +0200

    Add import/export settings/data

--- modules/settings.js
+++ modules/settings.js
@@ -35,6 +35,28 @@ export default class settings {
 
         let self = this;
 
+        let createDisplay = function(newDisplay) {
+            config.displays.push(newDisplay);
+            let view = new display(sharedIO, dispatcher, newDisplay, newDisplay.id, bundleManager);
+            view.changeBundle(newDisplay.bundle);
+            screenView.push(view);
+            adminView.push(new admin(sharedIO, dispatcher, view, newDisplay.id, bundleManager));
+        };
+
+        let removeDisplay = function(displayId) {
+            let displayIndex = config.displays.findIndex((d) => d.id === parseInt(displayId))
+            if (!displayIndex) {
+                cli.error('Removing Display failed, not found with id:' + displayId)
+                socket.emit("callback.error", "Removing Display failed");
+                socket.emit('callback.settingsReload');
+                return
+            }
+            config.displays.splice(displayIndex, 1);
+            let removedScreenView = screenView.splice(displayIndex, 1);
+            removedScreenView[0].destroy();
+            adminView.splice(displayIndex, 1);
+        };
+
         io.on("connection", function (socket) {
             cli.success("WS " + socket.conn.remoteAddress + " connect with id:" + socket.id);
 
@@ -62,12 +84,12 @@ export default class settings {
             socket.on('settings.renameDisplay', function (data) {
                 let display = config.displays.find((d) => d.id === parseInt(data.id))
                 if (!display) {
-                    cli.error('Renaming Display failed, not found with id:' + data.id)
+                    cli.error('Renaming Display failed, not found with id:' + data.id);
                     socket.emit("callback.error", "Renaming Display failed");
-                    return
+                    return;
                 }
-                display.name = data.name
-                settingsManager.saveSettings()
+                display.name = data.name;
+                settingsManager.saveSettings();
             });
 
             socket.on('settings.createDisplay', function (data) {
@@ -75,33 +97,17 @@ export default class settings {
                     name: 'New Display',
                     bundle: "default",
                     id: 0
-                }
+                };
                 if (config.displays.length > 0) {
-                    newDisplay.id = config.displays.reduce((p, c) => (p && p.id > c.id) ? p : c).id+1
+                    newDisplay.id = config.displays.reduce((p, c) => (p && p.id > c.id) ? p : c).id+1;
                 }
-                config.displays.push(newDisplay)
-
-                let view = new display(sharedIO, dispatcher, newDisplay, newDisplay.id, bundleManager);
-                view.changeBundle('default')
-                screenView.push(view);
-                adminView.push(new admin(sharedIO, dispatcher, view, newDisplay.id, bundleManager));
-                socket.emit('callback.settingsReload')
-                settingsManager.saveSettings()                
+                createDisplay(newDisplay);//, sharedIO, dispatcher, bundleManager);
+                socket.emit('callback.settingsReload');
+                settingsManager.saveSettings();  
             });
 
             socket.on('settings.removeDisplay', function (data) {
-                console.log(data)
-                let displayIdx = config.displays.findIndex((d) => d.id === parseInt(data.id))
-                if (!displayIdx) {
-                    cli.error('Removing Display failed, not found with id:' + data.id)
-                    socket.emit("callback.error", "Removing Display failed");
-                    socket.emit('callback.settingsReload')
-                    return
-                }
-                config.displays.splice(displayIdx, 1); 
-                let removedScreenView = screenView.splice(displayIdx, 1)
-                removedScreenView[0].destroy() 
-                adminView.splice(displayIdx, 1)   
+                removeDisplay(data.id);
                 socket.emit('callback.settingsReload')
                 settingsManager.saveSettings()
             });
@@ -121,18 +127,65 @@ export default class settings {
 
             socket.on('settings.editGuardrail', function (data) {
                 if (!Object.hasOwn(config.guardRails, data.id)) {
-                    cli.error('Editing guardrail failed, not found with id:' + data.id)
+                    cli.error('Editing guardrail failed, not found with id:' + data.id);
                     socket.emit("callback.error", "Editing guardrail failed");
                     return 
                 }
                 try {
-                    config.guardRails[parseInt(data.id)] = JSON.parse(data.value)
+                    config.guardRails[parseInt(data.id)] = JSON.parse(data.value);
                 } catch (e) {
-                    cli.error('Editing guardrail failed:' + e)
+                    cli.error('Editing guardrail failed:' + e);
                     socket.emit("callback.error", "Error saving guardrail, Please make sure it is a proper JSON.");
-                    return 'failed'
                 }
                 settingsManager.saveSettings()
+            });
+            socket.on('settings.requestExport', function () {
+                socket.emit("callback.exportSettings", settingsManager.getUsableConfig());
+            });
+            socket.on('settings.requestImport', function (data) {
+                try {
+                    const newSettings = JSON.parse(data);
+                    const newDisplays = newSettings.displays;
+                    //handle displays seperately as we need to create corresponding backend handlers
+                    delete newSettings.displays;
+                    const newDisplayIds = newDisplays.map((d) => d.id);
+                    const matchedDisplays = [];
+                    const removeDisplays = [];
+                    for (let i in config.displays) {
+                        const display = config.displays[i];
+                        const foundIdx = newDisplayIds.indexOf(display.id);
+                        if (foundIdx !== -1) {
+                            matchedDisplays.push({
+                                oldIndex: i,
+                                newConfig: newDisplays[i]
+                            });
+                            newDisplayIds.splice(foundIdx, 1);
+                        } else {
+                            removeDisplays.push(display.id);
+                        }
+                    }
+                    matchedDisplays.forEach((matchedDisplay) => {
+                        const display = config.displays[matchedDisplay.oldIndex];
+                        const newDisplay = matchedDisplay.newConfig;
+                        display.name = newDisplay.name;                        
+                        if (display.bundle !== newDisplay.bundle) {
+                            const view = screenView.find((sV) => sV.getDisplayId() === display.id)
+                            view.changeBundle(newDisplay.bundle);
+                        }
+                    });
+                    newDisplays.filter((d) => newDisplayIds.indexOf(d.id) !== -1).
+                        forEach(createDisplay);//(display) => createDisplay(display, sharedIO, dispatcher, bundleManager));
+                    removeDisplays.forEach(removeDisplay);
+                    settingsManager.importSettings(newSettings);
+                    socket.emit('callback.settingsReload');
+                } catch (e) {
+                    cli.error('Importing settings failed:' + e);
+                    socket.emit("callback.error", "Error importing settings, Please make sure it is a proper JSON.");
+                }                
+            });
+
+            socket.on('settings.requestExportData', async function () {
+                socket.emit("callback.exportData", await settingsManager.exportData());
             });
         }); // io
     }
